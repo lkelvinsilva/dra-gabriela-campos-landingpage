51ad7203a0a7377fe4f41ed9fb9d93a4f5f6a107
lkelvinsilva <lkelvinsilva@gmail.com>
2025-12-06 19:00:52 -0300

    Fix: remove merge conflict markers

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
-=======
+
 module.exports = {
 plugins: {
 tailwindcss: {},
 autoprefixer: {},
 },
 };
->>>>>>> 4f97506e04f5c44cd9b36bd2598a58c0b1003ef9
+
