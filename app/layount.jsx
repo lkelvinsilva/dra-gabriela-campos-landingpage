b102ff4f8435474c30a649f0910249a606472693
lkelvinsilva <lkelvinsilva@gmail.com>
2025-12-11 15:54:24 -0300

    Correção do menu mobile e botão hamburguer

--- app/layount.jsx
+++ app/layount.jsx
@@ -1,16 +1,21 @@
-import './globals.css'
-
+import "./globals.css";
 
 export const metadata = {
-title: 'Dra Gabriela Campos - Odontologia & Harmonização Facial',
-description: 'Atendimento especializado em odontologia estética e harmonização facial. Resultados naturais e personalizados.',
-}
-
+  title: "Dra Gabriela Campos - Odontologia & Harmonização Facial",
+  description:
+    "Atendimento especializado em odontologia estética e harmonização facial. Resultados naturais e personalizados.",
+};
 
 export default function RootLayout({ children }) {
-return (
-<html lang="pt-BR">
-<body>{children}</body>
-</html>
-)
+  return (
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/logo-gc.jpg" />
+      </head>
+
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  );
 }
